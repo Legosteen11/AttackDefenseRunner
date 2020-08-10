a32b174d6513a262b2a78c77a0da3c0869ea5ace
Wouter Doeland <git@wouterdoeland.nl>
2020-08-10 22:10:19 +0200

    Added simple form

--- AttackDefenseRunner/Pages/Index.cshtml.cs
+++ AttackDefenseRunner/Pages/Index.cshtml.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
 
 namespace AttackDefenseRunner.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public string Tag { get; set; }
+        
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            var tag = Tag;
+            
+            Log.Information("Tag is {tag}", tag);
         }
     }
 }
