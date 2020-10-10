0a8c8c4e7fac3c8d9c4abd429c900d5a8d9d032d
Wouter Doeland <git@wouterdoeland.nl>
2020-10-10 21:54:41 +0200

    Continued working on remote image manager

--- AttackDefenseRunner/Util/Docker/RemoteDockerImageManager.cs
+++ AttackDefenseRunner/Util/Docker/RemoteDockerImageManager.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using AttackDefenseRunner.Model;
+using AttackDefenseRunner.Util.Parsing;
 using AttackDefenseRunner.Util.Parsing.Json;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using ContainerList = System.Collections.Generic.List<AttackDefenseRunner.Model.DockerContainer>;
 
 namespace AttackDefenseRunner.Util.Docker
@@ -16,17 +20,26 @@ namespace AttackDefenseRunner.Util.Docker
         private readonly ADRContext _context;
         private readonly DockerTagManager _tagManager;
         private readonly IRemoteWorkerManager _workManager;
+        private readonly HttpClient _httpClient;
         private readonly List<IObserver<List<DockerContainer>>> _observers = new List<IObserver<ContainerList>>();
 
-        public RemoteDockerImageManager(ADRContext context, DockerTagManager tagManager, IRemoteWorkerManager workManager)
+        public RemoteDockerImageManager(ADRContext context, DockerTagManager tagManager, IRemoteWorkerManager workManager, HttpClient httpClient)
         {
             _context = context;
             _tagManager = tagManager;
             _workManager = workManager;
+            _httpClient = httpClient;
         }
 
-        public Task<DockerContainer> StartContainer(DockerTag tag)
+        public async Task<DockerContainer> StartContainer(DockerTag tag)
         {
+            await StopImage(tag.Tag);
+            
+            var worker = await NextWorker();
+
+            var jsonString = $"{{\"Tag\"=\"{tag.Tag}\"}}";
+
+            return await ApiPost<DockerContainer>(worker, Endpoint.UPDATE_IMAGE, jsonString);
         }
 
         public Task StopContainer(string id)
@@ -46,23 +59,54 @@ namespace AttackDefenseRunner.Util.Docker
 
         public async Task<UsageJson> GetUsage()
         {
-            UsageJson usage = new UsageJson
+            UsageJson totalUsage = new UsageJson
             {
                 MemorySet = false,
                 CpuSet = false
             };
 
-            foreach (var worker in await _workManager.GetWorkers())
+            foreach (var usage in await GetUsages())
             {
-                // TODO: Get usage and add it up
+                
             }
 
-            return usage;
+            return totalUsage;
         }
 
-        private Task<RemoteWorker> NextWorker()
+        private async Task<Dictionary<RemoteWorker, UsageJson>> GetUsages()
         {
-            
+            var usages = new Dictionary<RemoteWorker, UsageJson>();
+
+            foreach (var worker in await _workManager.GetWorkers())
+            {
+                usages.Add(worker, await ApiGet<UsageJson>(worker, Endpoint.USAGE_BASE));
+            }
+
+            return usages;
+        }
+
+        private async Task<RemoteWorker> NextWorker()
+            => (await GetUsages())
+                .OrderByDescending(c => c.Value.MemoryLeft)
+                .FirstOrDefault()
+                .Key;
+
+        private async Task<T> ApiGet<T>(RemoteWorker worker, string url)
+        {
+            var result = await _httpClient.GetAsync(worker.Host + url);
+
+            var content = result.Content.ToString();
+
+            return content == null ? default : JsonUtil.FromString<T>(content);
+        }
+
+        private async Task<T> ApiPost<T>(RemoteWorker worker, string url, string content)
+        {
+            var result = await _httpClient.PostAsync(worker.Host + url, new StringContent(content));
+
+            content = result.Content.ToString();
+
+            return content == null ? default : JsonUtil.FromString<T>(content);
         }
 
         private async Task Notify()
